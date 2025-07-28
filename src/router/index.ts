b098cf5d73cb3ae36a4f614a424511e08f5d861c
ApiLies <Apisit_Liesmann@cmu.ac.th>
2025-07-28 22:23:35 +0700

    Lab 3 Checkpoint 5.2
    Handle Network error

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,6 +7,7 @@ import EventEditView from '@/views/event/EditView.vue'
 import EventLayoutView from '@/views/event/LayoutView.vue'
 import StudentListView from '@/views/StudentListView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -47,6 +48,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: AboutView,
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView,
     },
     {
       path: '/404/:resource',
