2a9a2a4c349da46d3a2bfd98400044dcf17c3cd3
ApiLies <Apisit_Liesmann@cmu.ac.th>
2025-08-01 10:57:51 +0700

    Lab 5 Save Progress at Checkpoint 10.3

--- src/App.vue
+++ src/App.vue
@@ -7,48 +7,36 @@ const { message } = storeToRefs(store)
 </script>
 
 <template>
-  <div class="text-center front-sans text-gray-700 antialias">
+  <div class="text-center font-sans text-gray-700 antialias">
     <header>
-      <div id="flashMessage" v-if="message">
+      <div id="flashMessage" class="animate-fade" v-if="message">
         <h4>{{ message }}</h4>
       </div>
       <div class="wrapper">
-        <nav>
-          <RouterLink to="/">Event</RouterLink> | <RouterLink to="/about">About</RouterLink> |
-          <RouterLink to="/students">Student</RouterLink>
+        <nav class="py-6">
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'event-list-view' }"
+            >Event</RouterLink
+          >
+          |
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'about' }"
+            >About</RouterLink
+          >
+          |
+          <RouterLink
+            class="font-bold text-gray-700"
+            exact-active-class="text-green-500"
+            :to="{ name: 'students' }"
+            >Student</RouterLink
+          >
         </nav>
       </div>
     </header>
     <RouterView />
   </div>
 </template>
-
-<style>
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-
-h2 {
-  font-size: 20px;
-}
-@keyframes yellofade {
-  from {
-    background-color: yellow;
-  }
-  to {
-    background-color: transparent;
-  }
-}
-#flashMessage {
-  animation: yellowfade 3s ease-in-out;
-}
-</style>
